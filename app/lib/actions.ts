4aa83048bb82ea8935d1510d5759642bde4cb267
Ombre <oliverthek@gmail.com>
2024-01-26 09:22:04 +0100

    url jav 3

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -40,7 +40,7 @@ export async function createMessenger(formData: FormData) {
   }
 
   try {
-    await fetch(url + "/api/send", {
+    await fetch(url + "api/send", {
       method: "POST",
       body: JSON.stringify({ name, email, message }),
       headers: new Headers({
