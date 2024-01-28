123c2387ba7845957d9e6130a1e5bf5b5a842eed
Ombre <oliverthek@gmail.com>
2024-01-28 17:47:41 +0100

    error handling 2.3

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,13 +1,13 @@
-"use client";
+import { redirectBack } from "../lib/actions";
 
-export default function Error({ reset }: { reset: () => void }) {
+export default function Error() {
   return (
     <div className="container d-flex align-items-center flex-column py-4">
       <h2 className="text-center">
         Name must be 2 or more characters long, the email must be a valid email
         address, message must be 12 or more characters long.
       </h2>
-      <button className="btn btn-primary btn-xl" onClick={() => reset()}>
+      <button className="btn btn-primary btn-xl" onClick={() => redirectBack()}>
         Try again
       </button>
     </div>
