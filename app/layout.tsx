5d9d4b1c1514fcca158c4ada721ff4a3d59e1909
Ombre <oliverthek@gmail.com>
2024-01-30 11:20:06 +0100

    script added to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import "@/app/styles.css";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { Analytics } from "@vercel/analytics/react";
-import Head from "next/head";
 import Script from "next/script";
 
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
@@ -23,13 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <Head>
-        <Script type="text/javascript" src="../../public/scripts.js" />
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></Script>
-      </Head>
       <body id="page-top">
         {children}
         <Analytics />
+        <Script type="text/javascript" src="../../public/scripts.js" />
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></Script>
       </body>
     </html>
   );
